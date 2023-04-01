b79777f616a511e6090bbf642cd4174f5aa018b6
Varun Shringarpure <vshringarpure@horizontal.com>
2023-04-01 12:26:18 +0530

    Initial Commit - Integration of Study Lab Theme

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,65 @@
+
+
+<footer className="ftco-footer ftco-no-pt">
+  <div className="container">
+    <div className="row mb-5">
+      <div className="col-md pt-5">
+        <div className="ftco-footer-widget pt-md-5 mb-4">
+          <h2 className="ftco-heading-2">About</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <ul className="ftco-footer-social list-unstyled float-md-left float-lft">
+            <li className="ftco-animate"><a href="#"><span className="fa fa-twitter"></span></a></li>
+            <li className="ftco-animate"><a href="#"><span className="fa fa-facebook"></span></a></li>
+            <li className="ftco-animate"><a href="#"><span className="fa fa-instagram"></span></a></li>
+        </ul>
+    </div>
+</div>
+<div className="col-md pt-5">
+    <div className="ftco-footer-widget pt-md-5 mb-4 ml-md-5">
+      <h2 className="ftco-heading-2">Help Desk</h2>
+      <ul className="list-unstyled">
+        <li><a href="#" className="py-2 d-block">Customer Care</a></li>
+        <li><a href="#" className="py-2 d-block">Legal Help</a></li>
+        <li><a href="#" className="py-2 d-block">Services</a></li>
+        <li><a href="#" className="py-2 d-block">Privacy and Policy</a></li>
+        <li><a href="#" className="py-2 d-block">Refund Policy</a></li>
+        <li><a href="#" className="py-2 d-block">Call Us</a></li>
+    </ul>
+</div>
+</div>
+<div className="col-md pt-5">
+   <div className="ftco-footer-widget pt-md-5 mb-4">
+      <h2 className="ftco-heading-2">Recent Courses</h2>
+      <ul className="list-unstyled">
+        <li><a href="#" className="py-2 d-block">Computer Engineering</a></li>
+        <li><a href="#" className="py-2 d-block">Web Design</a></li>
+        <li><a href="#" className="py-2 d-block">Business Studies</a></li>
+        <li><a href="#" className="py-2 d-block">Civil Engineering</a></li>
+        <li><a href="#" className="py-2 d-block">Computer Technician</a></li>
+        <li><a href="#" className="py-2 d-block">Web Developer</a></li>
+    </ul>
+</div>
+</div>
+<div className="col-md pt-5">
+    <div className="ftco-footer-widget pt-md-5 mb-4">
+       <h2 className="ftco-heading-2">Have a Questions?</h2>
+       <div className="block-23 mb-3">
+         <ul>
+           <li><span className="icon fa fa-map-marker"></span><span className="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+           <li><a href="#"><span className="icon fa fa-phone"></span><span className="text">+2 392 3929 210</span></a></li>
+           <li><a href="#"><span className="icon fa fa-paper-plane"></span><span className="text">info@yourdomain.com</span></a></li>
+       </ul>
+   </div>
+</div>
+</div>
+</div>
+<div className="row">
+  <div className="col-md-12 text-center">
+
+    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+      Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i className="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+      <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+  </div>
+</div>
+</div>
+</footer>
