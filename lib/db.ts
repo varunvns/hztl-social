76d454549e341327fae7a4d879c206c57e3f5f2d
Chirag Khanna <ckhanna@horizontal.com>
2023-04-01 22:01:21 +0530

    changed connection string

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-    "mongodb+srv://sa:kZ6qNXw5hJM75rfsu@cluster0.1poyex2.mongodb.net/oauth?retryWrites=true&w=majority"
+    "mongodb+srv://sa:kZ6qNXw5hJM75rfu@cluster0.1poyex2.mongodb.net/oauth?retryWrites=true&w=majority"
   );
   return client;
 }
