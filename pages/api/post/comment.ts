dcab19b673699c6ef660b166aea1a9927645f01b
Varun Shringarpure <vshringarpure@horizontal.com>
2023-04-02 12:59:11 +0530

    Added CommentForm for Akshay

--- pages/api/post/comment.ts
+++ pages/api/post/comment.ts
@@ -11,10 +11,11 @@ async function handler(
   }
   const data = req.body;
   const { commentreceiverid, commentauthorid, comment } = data;
-  if ( (!Number.isInteger(commentreceiverid) && commentreceiverid < 0)
-    || (!Number.isInteger(commentauthorid) && commentauthorid < 0)
-    || !comment
-    ) {
+  if (
+    (!Number.isInteger(commentreceiverid) && commentreceiverid < 0) ||
+    (!Number.isInteger(commentauthorid) && commentauthorid < 0) ||
+    !comment
+  ) {
     res.status(422).json({ message: "Invalid Input" });
     return;
   }
@@ -22,24 +23,24 @@ async function handler(
   const client = await connectToDatabase();
   const db = client.db();
   //Make dynamic
- 
-  
-  const existingUser = await db.collection("employees").findOne({ id: commentauthorid });
+
+  const existingUser = await db
+    .collection("employees")
+    .findOne({ id: commentauthorid });
   if (!existingUser) {
     res.status(422).json({ message: "User does not exist" });
     client.close();
     return;
   }
   const result = await db.collection("comments").insertOne({
-    commentreceiverid:commentreceiverid,
-    commentauthorid:commentauthorid,
-    DateTime: Date.now(), 
-    comment: comment  
+    commentreceiverid: commentreceiverid,
+    commentauthorid: commentauthorid,
+    DateTime: Date.now(),
+    comment: comment,
   });
   console.log(result);
-  res.status(201).json({ message: "Saved comment to mongodb" });
+  res.status(201).json({ message: "" });
   client.close();
 }
-
 
 export default handler;
