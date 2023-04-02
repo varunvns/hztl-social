06cf963ea4a6bc14f7b809f7085c258fab2a8607
Varun Shringarpure <vshringarpure@horizontal.com>
2023-04-02 12:54:16 +0530

    Fixed the Javascript issue in solution and updating the components content

--- models/marketing/banner.ts
+++ models/marketing/banner.ts
@@ -1,12 +1,12 @@
 import { Banner } from "@/lib/types";
 
 const banner = new Banner(
-  "Welcome to StudyLab",
-  "We Are Online Platform For Make Learn",
-  "/Theme/images/bg_1.jpg",
+  "Welcome to Horizontal Social",
+  "An Online Platform of Horizontal, For Horizontal, By Horizontal",
+  "/Theme/images/hztl-office.jpg",
   "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia",
-  "Our Course",
-  "#",
+  "Give Shoutout",
+  "/shoutout",
   "Learn More",
   "#"
 );
