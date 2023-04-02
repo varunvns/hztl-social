19e50dd817313028a73388890d210b8eca42a826
Chirag Khanna <ckhanna@horizontal.com>
2023-04-02 18:32:22 +0530

    promo changes

--- models/marketing/promo.ts
+++ models/marketing/promo.ts
@@ -6,48 +6,6 @@ const promo = new Promo(
   "#",
   "See All Categories",
   [
-    {
-      id: 1,
-      title: "IT & Software",
-      text: "100 course",
-      link: "#",
-      image: "/Theme/images/work-1.jpg",
-    },
-    {
-      id: 2,
-      title: "IT & Software",
-      text: "100 course",
-      link: "#",
-      image: "/Theme/images/work-9.jpg",
-    },
-    {
-      id: 3,
-      title: "IT & Software",
-      text: "100 course",
-      link: "#",
-      image: "/Theme/images/work-3.jpg",
-    },
-    {
-      id: 4,
-      title: "IT & Software",
-      text: "100 course",
-      link: "#",
-      image: "/Theme/images/work-1.jpg",
-    },
-    {
-      id: 5,
-      title: "IT & Software",
-      text: "100 course",
-      link: "#",
-      image: "/Theme/images/work-9.jpg",
-    },
-    {
-      id: 6,
-      title: "IT & Software",
-      text: "100 course",
-      link: "#",
-      image: "/Theme/images/work-3.jpg",
-    },
   ]
 );
 
